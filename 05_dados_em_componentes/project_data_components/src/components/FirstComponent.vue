525dba20d3efdc3b09e2f21cf7d34162c28b61ba
jpcp0614 <jpcp0614@gmail.com>
2023-07-31 20:28:54 -0300

    Utilizando o data() no script

--- 05_dados_em_componentes/project_data_components/src/components/FirstComponent.vue
+++ 05_dados_em_componentes/project_data_components/src/components/FirstComponent.vue
@@ -1,9 +1,18 @@
 <template>
-  <h1>Olá Vue!</h1>
+  <div>
+    <h1>{{ framework }}</h1>
+    <h3>A linguagem utilizada é {{ language }}</h3>
+  </div>
 </template>
 
 <script>
   export default {
     name: 'FirstComponent',
+    data () {
+      return {
+        language: 'JavaScript',
+        framework: 'Vue'
+      }
+    }
   }
 </script>
