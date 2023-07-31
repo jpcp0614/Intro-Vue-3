78867c0160723dee43d68d0a0e340b77a92b6156
jpcp0614 <jpcp0614@gmail.com>
2023-07-31 19:45:38 -0300

    Projeto criado

---
+++ 05_dados_em_componentes/project_data_components/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <FirstComponent />
+</template>
+
+<script>
+  import FirstComponent from './components/FirstComponent';
+
+  export default {
+    name: 'App',
+    components: {
+      FirstComponent
+    }
+  }
+</script>
