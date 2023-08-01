410dcd06f816e080ffd5bfafa4a7134dc376ff8f
jpcp0614 <jpcp0614@gmail.com>
2023-08-01 19:51:18 -0300

    Utilizando o Info no Person

--- 07_hierarquia_componentes/project_component_hierarchy/src/components/Person.vue
+++ 07_hierarquia_componentes/project_component_hierarchy/src/components/Person.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
     <h2>Esta é a descrição da pessoa: {{ name }}</h2>
+    <Info />
   </div>
 </template>
 
 <script>
+
+  import Info from './Info';
+
   export default {
     name: 'Person',
     data () {
       return {
         name: 'John',
       }
+    },
+    components: {
+      Info,
     }
   }
 </script>
