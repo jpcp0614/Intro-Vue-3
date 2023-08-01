d814f380173ea963ffcf932a148c0da11186c7aa
jpcp0614 <jpcp0614@gmail.com>
2023-08-01 19:43:48 -0300

    Utilizando o Person no App

--- 07_hierarquia_componentes/project_component_hierarchy/src/App.vue
+++ 07_hierarquia_componentes/project_component_hierarchy/src/App.vue
@@ -2,18 +2,21 @@
   <div>
     <FirstComponent />
     <LifeCycle />
+    <Person />
   </div>
 </template>
 
 <script>
   import FirstComponent from './components/FirstComponent';
   import LifeCycle from './components/LifeCycle';
+  import Person from './components/Person';
 
   export default {
     name: 'App',
     components: {
       FirstComponent,
-      LifeCycle
+      LifeCycle,
+      Person,
     }
   }
 </script>
