20b3bdbcbb6b304e1dff95fd0134433968a031cd
jpcp0614 <jpcp0614@gmail.com>
2023-07-31 21:03:52 -0300

    Cria o componente e as Hooks: created() e mounted()

---
+++ 06_lifecycle_hooks/project_lifecycle_hooks/src/components/LifeCycle.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>My technology is: {{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LifeCycle',
+  data () {
+    return {
+      name: 'I don\'t know, yet!'
+    };
+  },
+  created () { //* hook
+    setTimeout(() => {
+      this.name = 'React'
+    }, 2000);
+  },
+  mounted () { //* hook superior ao created
+    setTimeout(() => {
+      this.name = 'Vue'
+    }, 3000);
+  }
+}
+</script>
