5bf41696a546df85765328c445a60b69cf2f4eff
jpcp0614 <jpcp0614@gmail.com>
2023-08-01 19:43:33 -0300

    Component Person

---
+++ 07_hierarquia_componentes/project_component_hierarchy/src/components/Person.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Esta é a descrição da pessoa: {{ name }}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Person',
+    data () {
+      return {
+        name: 'John',
+      }
+    }
+  }
+</script>
