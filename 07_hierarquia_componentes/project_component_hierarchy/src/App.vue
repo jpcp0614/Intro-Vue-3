49552f4729c3bad29bf17705a6ec8bc0caaccd90
jpcp0614 <jpcp0614@gmail.com>
2023-08-01 19:26:28 -0300

    Projeto criado

---
+++ 07_hierarquia_componentes/project_component_hierarchy/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <FirstComponent />
+    <LifeCycle />
+  </div>
+</template>
+
+<script>
+  import FirstComponent from './components/FirstComponent';
+  import LifeCycle from './components/LifeCycle';
+
+  export default {
+    name: 'App',
+    components: {
+      FirstComponent,
+      LifeCycle
+    }
+  }
+</script>
