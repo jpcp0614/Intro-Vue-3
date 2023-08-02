6a4640516dd85256f2c26b82e0154ae7eede1f40
jpcp0614 <jpcp0614@gmail.com>
2023-08-02 19:24:39 -0300

    Cria Projeto

---
+++ 08_diretivas/project_directives/src/components/Info.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Estou estudando no momento.</p>
+    <p>Utilizo as seguinte tecnologias:</p>
+    <ul>
+      <li>JavaScript</li>
+      <li>VueJS</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Info'
+}
+</script>
