522b3d74e0913747dbaffed43bd7876c5e858e69
jpcp0614 <jpcp0614@gmail.com>
2023-08-02 19:36:08 -0300

    v-if, v-else e v-show

--- 08_diretivas/project_directives/src/components/Info.vue
+++ 08_diretivas/project_directives/src/components/Info.vue
@@ -1,16 +1,27 @@
 <template>
   <div>
-    <p>Estou estudando no momento.</p>
+    //* v-else tem que ficar sempre abaixo do v-if
+    <p v-if="studying">Estou estudando no momento.</p>
+    <p v-else>Não estou estudando mais</p>
     <p>Utilizo as seguinte tecnologias:</p>
     <ul>
       <li>JavaScript</li>
       <li>VueJS</li>
     </ul>
+    //* v-show simplifica o v-if/v-else
+    <p v-show="show_email">Mande um mensagem para: {{ email }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Info'
+  name: 'Info',
+  data () {
+    return {
+      studying: true,
+      show_email: true,
+      email: 'info@email.com'
+    }
+  }
 }
 </script>
