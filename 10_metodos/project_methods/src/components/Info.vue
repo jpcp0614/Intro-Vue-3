1a8eaabb629d5cbec56feb8e4e57bbfaf74cbf55
jpcp0614 <jpcp0614@gmail.com>
2023-08-02 20:20:24 -0300

    Cria Projeto

---
+++ 10_metodos/project_methods/src/components/Info.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <!-- v-else tem que ficar sempre abaixo do v-if -->
+    <p v-if="studying">Estou estudando no momento.</p>
+    <p v-else>Não estou estudando mais</p>
+    <p>Utilizo as seguinte tecnologias:</p>
+    <ul>
+      <li>JavaScript</li>
+      <li>VueJS</li>
+    </ul>
+    <!-- v-show simplifica o v-if/v-else -->
+    <p v-show="show_email">Mande um mensagem para: {{ email }}</p>
+    <br>
+    <p>Para acessar o meu portfolio, <a v-bind:href="my_portfolio" target="_blank">clique aqui</a> </p>
+    <br>
+    <Picture />
+  </div>
+</template>
+
+<script>
+
+import Picture from './Picture';
+
+export default {
+  name: 'Info',
+  data () {
+    return {
+      studying: true,
+      show_email: true,
+      email: 'info@email.com',
+      my_portfolio: 'https://google.com',
+    }
+  },
+  components: {
+    Picture,
+  }
+}
+</script>
