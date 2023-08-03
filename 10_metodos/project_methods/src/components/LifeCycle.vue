51da29e2ccdfb9a548c5d331a60f8f498740a63f
jpcp0614 <jpcp0614@gmail.com>
2023-08-02 21:04:06 -0300

    Método associado aos LifeCycle Hooks

--- 10_metodos/project_methods/src/components/LifeCycle.vue
+++ 10_metodos/project_methods/src/components/LifeCycle.vue
@@ -15,12 +15,21 @@ export default {
   created () { //* hook
     setTimeout(() => {
       this.name = 'React'
+      this.LifeCycle ();
     }, 2000);
+
   },
   mounted () { //* hook superior ao created
     setTimeout(() => {
       this.name = 'Vue'
+      this.LifeCycle ();
     }, 3000);
+
+  },
+  methods: {
+    LifeCycle () {
+      console.log("Executou")
+    }
   }
 }
 </script>
