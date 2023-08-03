d9b000e053724344ee3dd7f8b4614749fe670c3b
jpcp0614 <jpcp0614@gmail.com>
2023-08-02 21:10:34 -0300

    Cria Projeto

---
+++ 11_css_scoped_e_global/project_css/src/components/Person.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Esta é a descrição da pessoa: {{ name }}</h2>
+    <Info />
+    <Form />
+  </div>
+</template>
+
+<script>
+
+  import Info from './Info';
+    import Form from './Form';
+
+  export default {
+    name: 'Person',
+    data () {
+      return {
+        name: 'John',
+      }
+    },
+    components: {
+      Info,
+      Form,
+    }
+  }
+</script>
