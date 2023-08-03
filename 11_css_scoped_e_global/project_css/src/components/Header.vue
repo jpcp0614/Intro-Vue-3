1cdd57c190f480f401a512e0cdc8543ec32210af
jpcp0614 <jpcp0614@gmail.com>
2023-08-02 21:27:45 -0300

    Cria o Header

---
+++ 11_css_scoped_e_global/project_css/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ul>
+      <li><a href="">Home</a></li>
+      <li><a href="">Projetos</a></li>
+      <li><a href="">Contato</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+  ul {
+    list-style: none;
+    display: flex;
+  }
+
+  li {
+    margin-right: 10px;
+  }
+
+  a {
+    color: #ddd;
+    text-decoration: none;
+    transition: .5s;
+  }
+
+  a:hover {
+    color: turquoise;
+  }
+</style>
