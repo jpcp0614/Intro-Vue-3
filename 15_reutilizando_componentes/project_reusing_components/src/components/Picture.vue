f47c41cc155bd16a59e27e63cd036a019690d901
jpcp0614 <jpcp0614@gmail.com>
2023-08-04 17:32:57 -0300

    Cria Projeto

---
+++ 15_reutilizando_componentes/project_reusing_components/src/components/Picture.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- : é a mesma coisa que colocar v-bind -->
+    <img :src="avatar" :alt="describe">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Picture',
+  data () {
+    return {
+      avatar: '/img/avatar.png',
+      describe: 'John Smith'
+    }
+  }
+}
+
+</script>
