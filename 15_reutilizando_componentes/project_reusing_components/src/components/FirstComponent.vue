6d00bd619ea4df36d8e6c8a40a2671298b21bf7d
jpcp0614 <jpcp0614@gmail.com>
2023-08-04 17:47:31 -0300

    Reusing

--- 15_reutilizando_componentes/project_reusing_components/src/components/FirstComponent.vue
+++ 15_reutilizando_componentes/project_reusing_components/src/components/FirstComponent.vue
@@ -4,17 +4,21 @@
     <h3>The language used is: {{ language }}</h3>
 
     <MultiplyEvents />
+    <Reusing />
+    <Reusing />
   </div>
 </template>
 
 <script>
 
   import MultiplyEvents from './MultiplyEvents';
+  import Reusing from './Reusing';
 
   export default {
     name: 'FirstComponent',
     components: {
       MultiplyEvents,
+      Reusing,
     },
     data () {
       return {
--- 15_reutilizando_componentes/project_reusing_components/src/components/Picture.vue
+++ 15_reutilizando_componentes/project_reusing_components/src/components/Picture.vue
@@ -2,13 +2,19 @@
   <div>
     <!-- : é a mesma coisa que colocar v-bind -->
     <img :src="avatar" :alt="describe">
+    <Reusing />
   </div>
 </template>
 
 <script>
 
+import Reusing from './Reusing';
+
 export default {
   name: 'Picture',
+  components: {
+    Reusing,
+  },
   data () {
     return {
       avatar: '/img/avatar.png',
