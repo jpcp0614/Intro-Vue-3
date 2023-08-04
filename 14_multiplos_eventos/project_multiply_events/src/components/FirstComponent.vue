235e475b0a5f64656d925244891d41e9785b16dd
jpcp0614 <jpcp0614@gmail.com>
2023-08-03 22:16:50 -0300

    Cria Projeto

---
+++ 14_multiplos_eventos/project_multiply_events/src/components/FirstComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ framework }}</h1>
+    <h3>The language used is: {{ language }}</h3>
+
+    <MultiplosEventos />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'FirstComponent',
+    data () {
+      return {
+        language: 'JavaScript',
+        framework: 'Vue'
+      }
+    }
+  }
+</script>
