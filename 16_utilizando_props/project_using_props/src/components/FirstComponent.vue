a3a3eac7ae63add5fdf579316ea7abbeafd55ad8
jpcp0614 <jpcp0614@gmail.com>
2023-08-04 17:51:32 -0300

    Cria Projeto

---
+++ 16_utilizando_props/project_using_props/src/components/FirstComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ framework }}</h1>
+    <h3>The language used is: {{ language }}</h3>
+
+    <MultiplyEvents />
+    <Reusing />
+    <Reusing />
+  </div>
+</template>
+
+<script>
+
+  import MultiplyEvents from './MultiplyEvents';
+  import Reusing from './Reusing';
+
+  export default {
+    name: 'FirstComponent',
+    components: {
+      MultiplyEvents,
+      Reusing,
+    },
+    data () {
+      return {
+        language: 'JavaScript',
+        framework: 'Vue'
+      }
+    }
+  }
+</script>
---
+++ 16_utilizando_props/project_using_props/src/components/MultiplyEvents.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button @click="primeiro(txt1, $event), segundo(txt2, $event), terceiro()">Ativar Múltiplos Eventos</button>
+    <p>{{ multiplos_eventos }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MultiplyEvents',
+  data() {
+    return {
+      multiplos_eventos: "",
+      txt1: "Primeiro Evento",
+      txt2: "Segundo Evento",
+    }
+  },
+  methods: {
+    primeiro(txt, e) {
+      this.multiplos_eventos = `${txt} Ativado!`
+      console.log("Clicou")
+    },
+    segundo(txt, e) {
+      setTimeout(() => {
+        this.multiplos_eventos = `${txt} Ativado!`;
+        console.log("Alterou sozinho 1000ms depois");
+      }, 1000);
+    },
+    terceiro() {
+      setTimeout(() => {
+        this.multiplos_eventos = "";
+        console.log("Apagou sozinho 2000ms depois")
+      }, 2000);
+    }
+  }
+}
+</script>
+
+<style scoped>
+  div {
+    margin: 100px;
+  }
+</style>
